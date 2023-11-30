95ed41c1a939472b2857b9fd4e71ee8b974f4e19
pablo <pablo.velezleon1@icloud.com>
2023-11-29 21:41:22 -0500

    taller 07 / ejercicios de pablo

---
+++ Solucion/Ejercicio01/src/Ejercicio01/ejercicio01.java
@@ -0,0 +1,19 @@
+package javaapplication40;
+public class JavaApplication40 {
+    public static void main(String[] args) {
+        int num, deno, i, n;
+        num = 1;
+        deno = 10;
+        i = 1;
+        n = 6;
+        while (i <= n) {
+            System.out.println("" + num + "/" + "" + deno);
+            num = (num + 1);
+            deno = (deno + 1);
+            i = i + 1;
+        }
+    }
+}
+    
+    
+
